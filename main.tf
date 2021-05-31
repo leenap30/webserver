422a60d222108c8ea6c9a6b34b2abf7dfb666aea
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-31 10:57:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "web" {
   ami           =  "ami-0d5eff06f840b45e9"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web.id]
   user_data         = <<EOF
 #!/bin/sh
