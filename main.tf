3426c0fc173d5c319ad2d33e11da2fb9029ac835
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-31 11:39:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,4 @@
-provider "aws"{
-region = "us-east-1"
-}
-
-resource "aws_instance" "web" {
-  ami           =  "ami-0d5eff06f840b45e9"
-  instance_type = "t2.micro"
-  security_groups   = ["${aws_security_group.webservers-ssh-http.name}"]
-  key_name = "keyec2"
-  user_data         = <<EOF
-                      #!/bin/sh
-                      yum -y update
-                      yum -y install httpd
-                      echo "<h2>Built by Terraform</h2>" >> /var/www/html/index.html
-                      service httpd start
-                      chkconfig httpd on
-                      EOF
-    tags = {
-    Name = "Webserver Built by Terraform"
-  }
-}
-
-resource "aws_security_group" "webservers-ssh-http" {
-  name        = "webservers-ssh-http"
-  description = "allow ssh and http traffic"
-
- ingress {
-    description      = "for webserver"provider "aws" {
+provider "aws" {
   region = "us-east-2"
 }
 resource "aws_security_group" "webservers-ssh-http" {
@@ -56,7 +29,7 @@ resource "aws_security_group" "webservers-ssh-http" {
 }
 
 resource "aws_instance" "webservers" {
-  ami               = "ami-077e31c4939f6a2f3"
+  ami               = "ami-0d5eff06f840b45e9"
   instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
   security_groups   = ["${aws_security_group.webservers-ssh-http.name}"]
